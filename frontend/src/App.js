e2a2ca9afbe034c41fc8d4aae0eb483d5e5198d4
Chris Nguyen <ductuan1999@gmail.com>
2020-10-07 21:45:56 +1100

    finalize again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './containers/Home';
-import NotFound from './containers/NotFound';
 
 
 function App() {
